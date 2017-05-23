f5323228e15f3b9d6e7ddfd9224e403fbffeb34f
Billy Bernhardt <wpbernha19@wabash.edu>
2017-05-23 11:10:06 -0400

    Render the 4 articles using map

--- src/AroundtheRealm.js
+++ src/AroundtheRealm.js
@@ -1,20 +1,60 @@
 import React, { Component } from 'react'
-import IndyOrcs from './IndyOrcs'
-import Mountain from './Mountain'
-import MoreGold from './MoreGold'
-import Hobbits99 from './Hobbits99'
+// import IndyOrcs from './IndyOrcs'
+// import Mountain from './Mountain'
+// import MoreGold from './MoreGold'
+// import Hobbits99 from './Hobbits99'
 import './AroundtheRealm.css'
 
 class AroundtheRealm extends Component {
+  constructor() {
+    super()
+    this.state = {
+      articles: [
+        {
+          title: "Single Orcs in Indianapolis",
+          href: "#",
+          img: "https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif",
+          alt: "orc",
+        },
+        {
+          title: "You won't believe what's under this mountain",
+          href: "#",
+          img: "https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg",
+          alt: "mountain",
+        },
+        {
+          title: "Mine 20% more gold with One Weird Trick",
+          href: "#",
+          img: "http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg",
+          alt: "gold",
+        },
+        {
+          title: "Surprise for Indiana Hobbits born before 1999",
+          href: "#",
+          img: "http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg",
+          alt: "hobbit",
+        }
+      ],
+    }
+  }
+
+renderArticle(article) {
+    return (
+      <div className="small-6 medium-3 columns other-article">
+        <a href={article.href}>
+          <img src={article.img} alt={article.alt}/>
+          <p>{article.title}</p>
+        </a>  
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="AroundtheRealm">
         <div className="small-12 columns other-articles">
         <h2>From around the Realm</h2>
-            <IndyOrcs />
-            <Mountain />
-            <MoreGold />
-            <Hobbits99 />
+            {this.state.articles.map(this.renderArticle)}
         </div>
       </div>
     )
